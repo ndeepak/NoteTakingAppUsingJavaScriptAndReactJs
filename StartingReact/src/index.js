d2d00876f45526ae17d843cea3a1b6d01abee075
Deepak <nagarkotideepak9843@gmail.com>
2022-07-08 16:54:20 +0545

    JSX HTML

--- StartingReact/src/index.js
+++ StartingReact/src/index.js
@@ -1,3 +1,11 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+
+var name = "Deepak Nagarkoti";
+ReactDOM.render(
+  <div>
+    <h1 class="heading">Hello {name}!</h1>
+    <p>This is a react js bootcamp</p>
+  </div>,
+  document.getElementById("root")
+);
