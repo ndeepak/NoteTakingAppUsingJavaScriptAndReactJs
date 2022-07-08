9d0494090b868c837d282a572cb814c7df9c4bed
Deepak <nagarkotideepak9843@gmail.com>
2022-07-08 23:36:54 +0545

    NoteTakingAppDevelopment

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Header from "./"
+function App() {
+  return <h1 className="heading">Hello Deepak Nagarkoti!</h1>;
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React Bootcamp</h1>
+      <p>
+        This is the react bootcamp for making note taking app. It is very useful
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
